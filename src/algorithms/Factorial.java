b40e4c6c566c3febf804994591146dd13b28bece
ColinGeukes <ColinGeukes@outlook.com>
2016-09-25 22:07:48 +0200

    Added Factorial.

--- src/algorithms/Factorial.java
+++ src/algorithms/Factorial.java
@@ -8,6 +8,7 @@ public class Factorial {
 	 * Method that returns the factorial value of a given value.
 	 * @param n - the number you want to get the factorial value from.
 	 * @return the factorial value of n.
+	 * @throws IllegalArgumentException if n is a negative value.
 	 */
 	public static int factorial(int n){
 		if(n < 0)	throw new IllegalArgumentException("int n needs cannot be a negative value.");
